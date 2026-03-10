03637c0935d6a3a71896f762d38f9d93caef4267
Jakub-Chovanec <jakub.chovanec@frengp.cz>
2026-03-10 12:24:30 +0000

    doplnění cvičení

--- lekce/07-matematika.php
+++ lekce/07-matematika.php
@@ -98,8 +98,47 @@ FUNKCE – DOPLŇTE ŘEŠENÍ
 
 function getTriangleAngleType(float $a, float $b, float $c): string
 {
-	// TODO: doplňte řešení
+	$sides = [$a, $b, $c];
+
+    sort($sides);
+
+    $a = $sides[0];
+    $b = $sides[1];
+    $c = $sides[2];
+
+    $left = $c * $c;
+    $right = $a * $a + $b * $b;
+
+    if ($left == $right) {
+        return "pravoúhlý";
+    }
+
+    if ($left < $right) {
+        return "ostroúhlý";
+    }
+
+    return "tupoúhlý";
 }
+
+
+// nastavení stran
+$a = 3;
+$b = 4;
+$c = 5;
+
+
+// výpis stran
+echo "Strana a: $a cm\n";
+echo "Strana b: $b cm\n";
+echo "Strana c: $c cm\n\n";
+
+
+// zavolání funkce
+$type = getTriangleAngleType($a, $b, $c);
+
+
+// výpis typu
+echo "Trojúhelník je: $type";
 
 
 function getHeightToA(float $a, float $content): float
